076122c69fecb2b3092ad4bb65101d875f3d29a3
JimmieB <jimbe263@student.liu.se>
2014-04-08 16:07:27 +0200

    Database class is done, Block/Order class update

--- TimeManagement/src/com/example/timemanagement/model/Block.java
+++ TimeManagement/src/com/example/timemanagement/model/Block.java
@@ -5,26 +5,71 @@ public class Block {
 	private int ID; // primary key
 	private int orderID; // foreign key
 	
-	private int start;
-	private int stop;
+	private long start;
+	private long stop;
 	
 	public Block() {
 		// Empty constructor
 	}
 	
-	public Block(int s) {
+	public Block(long s) {
 		// Start constructor
 		this.start = s;
 		this.stop = 0;
 		this.orderID = 0;
 	}
+
+	public Block(int orderID, long s) {
+		// Start constructor
+		this.start = s;
+		this.stop = 0;
+		this.orderID = orderID;
+	}
+
+	public Block(int orderID, long start, long stop) {
+		// Start constructor
+		this.start = start;
+		this.stop = stop;
+		this.orderID = orderID;
+	}
+
 	
-	public void setStop(int s) {
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public int getOrderID() {
+		return orderID;
+	}
+
+	public void setOrderID(int orderID) {
+		this.orderID = orderID;
+	}
+
+	public long getStart() {
+		return start;
+	}
+
+	public void setStart(long start) {
+		this.start = start;
+	}
+
+	public long getStop() {
+		return stop;
+	}
+	
+	public void setStop(long s) {
 		this.stop = s;
 	}
 	
+	@Override
 	public String toString() {
-		return "Block, start = " + this.start + ", stop =  " + this.stop + ", orderID = " + this.orderID;
+		return "Block [ID=" + ID + ", orderID=" + orderID + ", start=" + start
+				+ ", stop=" + stop + "]";
 	}
 	
 }
