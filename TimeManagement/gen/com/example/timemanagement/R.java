3e43fd6e7a055aa35e5e92835e0ade9d4b16de05
annfl042 <annfl042@student.liu.se>
2014-04-10 15:14:07 +0200

    satistics view

--- TimeManagement/gen/com/example/timemanagement/R.java
+++ TimeManagement/gen/com/example/timemanagement/R.java
@@ -56,7 +56,8 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_new_order=0x7f030001;
-        public static final int activity_timestamp=0x7f030002;
+        public static final int activity_statistics=0x7f030002;
+        public static final int activity_timestamp=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
