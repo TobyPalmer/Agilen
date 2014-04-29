52091ecafff9bb80c5049cb65b3225faa6c776ba
Marcus <marla061@student.liu.se>
2014-04-29 14:00:48 +0200

    Implemented the ability to check Tasks in ListActivity. Also Colorcoded

--- TimeManagement/src/com/example/timemanagement/ListActivity.java
+++ TimeManagement/src/com/example/timemanagement/ListActivity.java
@@ -1,6 +1,8 @@
 
 package com.example.timemanagement;
 
+
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -9,6 +11,7 @@ import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
 
+import android.content.*;
 import android.annotation.TargetApi;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Build;
@@ -18,6 +21,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -29,10 +34,12 @@ import com.example.timemanagement.R.color;
 import com.example.timemanagement.model.Block;
 import com.example.timemanagement.model.Order;
 import com.example.timemanagement.sqlite.SQLiteMethods;
-
 import com.example.timemanagement.R;
+import com.example.timemanagement.customadapters.*;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.graphics.Typeface;
 
 
@@ -60,12 +67,13 @@ import android.graphics.Typeface;
 		String s, dateString;
 		Block b;
 		int n = 0;
-		long today, day_next;
+		long today, day_next, stop, start;
 		private Date d;
 		private ListView l_view;
 		private TextView day;
-		private ArrayAdapter<String> listAdapter;
+		private CustomListAdapter1 listAdapter;
 		private ArrayList<String> blockList;
+		private ArrayList<Integer> blockStatesList;
 		private SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 		
 		@Override
@@ -77,13 +85,26 @@ import android.graphics.Typeface;
 			// Show the Up button in the action bar.
 			setupActionBar();
 			
+			//Sets stop to the end of this day.
+			Calendar cal = Calendar.getInstance();
+			cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), 
+					cal.get(Calendar.DAY_OF_MONTH), 23, 59);
+			stop = cal.getTimeInMillis();
+			
+			//Sets start to the start of this day.
+			cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), 
+					cal.get(Calendar.DAY_OF_MONTH), 0, 0);
+			start = cal.getTimeInMillis();
+			
 			//Create a list of all the blocks
-	    	bList = MainActivity.db.getAllBlocks();
+	    	//BList = MainActivity.db.getAllBlocks();
+			bList = MainActivity.db.getBlocksBetweenDate(start, stop);
 			
 			l_view = (ListView) findViewById(R.id.l_view);
 			day = (TextView) findViewById(R.id.day);
 			
-			blockList = new ArrayList<String>();		
+			blockList = new ArrayList<String>();
+			blockStatesList = new ArrayList<Integer>();
 			
 			//Calendar cal = new GregorianCalendar();
 			today = System.currentTimeMillis();
@@ -150,18 +171,98 @@ import android.graphics.Typeface;
 	    		}
 	    		
 	    		s += " - " + b.printTime();
-
+	    		
+	    		if(b.getChecked() == 1){
+	    			s += " - Checked! ";
+	    		}
+	    		
+	    		
 	    	    if(b.toDateString().equals(dateString))
 	    	    {
 	    	    	blockList.add(s);
+	    	    	if(!(b.getOrderID() == 0)){
+	    	    		blockStatesList.add(b.getChecked());
+	    	    	}
+	    	    	else{
+	    	    		blockStatesList.add(2);
+	    	    	}
+	    	    	
 	    	    }
 	    	}
 	    	
-	    	listAdapter = new ArrayAdapter<String>(this,R.layout.listrow, blockList);
-	    	l_view.setAdapter(listAdapter);
+	    	//listAdapter = new ArrayAdapter<String>(this,R.layout.listrow, blockList);
+	    	listAdapter = new CustomListAdapter1(this,R.layout.listrow, blockList, blockStatesList);
+	    	l_view.setAdapter(listAdapter);	    	
+	    	l_view.setOnItemClickListener(new OnItemClickListener()
+	    	{
+	    		
+	    		@Override 
+	    		public void onItemClick(AdapterView<?> adapter, View v, final int position, long arg3)
+	    		{
+	    			String value = (String)adapter.getItemAtPosition(position);
+	    			try{
+	    				if(!(bList.get(position).getOrderID() == 0)){
+	    					bList.get(position).setChecked(switchChecked(bList.get(position).getChecked()));	    				
+		    				blockStatesList.set(position, bList.get(position).getChecked());
+		    				listAdapter.setBlockStatesList(blockStatesList);
+		    				Log.e("Changed item checked to: ", Integer.toString(bList.get(position).getChecked()));
+		    				MainActivity.db.putBlock(bList.get(position));
+	    				}
+	    				else{
+	    					AlertDialog.Builder noOrderNrDialogBuilder = new AlertDialog.Builder(l_view.getContext());
+	    					noOrderNrDialogBuilder.setMessage("Denna posten saknar order nr!\nVill du redigera detta?");
+	    					noOrderNrDialogBuilder.setCancelable(true);
+	    					noOrderNrDialogBuilder.setPositiveButton("Ja", 
+	    							new DialogInterface.OnClickListener() {
+										
+										@Override
+										public void onClick(DialogInterface dialog, int which) {
+											Intent i = new Intent(getApplicationContext(), NewOrderActivity.class);
+								        	
+								        	i.putExtra("Block", bList.get(position));
+								        	i.putExtra("String", "editBlock");								        	        	
+								        	startActivity(i);											
+										}
+									});
+	    					
+	    					noOrderNrDialogBuilder.setNegativeButton("Nej", 
+	    							new DialogInterface.OnClickListener() {										
+										@Override
+										public void onClick(DialogInterface dialog, int which) {
+											dialog.cancel();
+										}
+									});
+	    					AlertDialog alertNoOrder = noOrderNrDialogBuilder.create();
+	    					alertNoOrder.show();
+	    				}
+	    				
+	    				
+	    			}
+	    			catch (Exception e){
+	    				Log.e("Något blev fel! At position:",Integer.toString(position));
+	    			}
+	    			Log.e("Värde i listan: ",value);
+	    			Log.e("Position i listan: ",Integer.toString(position));
+	    			iterateBlocks(getDateString());
+	    			
+	    		}
+	    	});
 	    	day.setText(dateString);
 		}
-
+		
+		private String getDateString(){
+			return dateString;			
+		}
+		
+		private int switchChecked (int value){
+			if(value > 0){
+				return 0;
+			}
+			else{
+				return 1;
+			}
+		}
+		
 		/**
 		 * Set up the {@link android.app.ActionBar}, if the API is available.
 		 */
