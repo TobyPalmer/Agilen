58a9807ba1244f0c4ea7cc28242c95743c6d9324
JimmieB <jimbe263@student.liu.se>
2014-05-08 13:06:28 +0200

    Minor bug fix

--- TimeManagement/src/com/example/timemanagement/BackupActivity.java
+++ TimeManagement/src/com/example/timemanagement/BackupActivity.java
@@ -10,7 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class BackupActivity extends Activity {
+public class BackupActivity extends MainActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
