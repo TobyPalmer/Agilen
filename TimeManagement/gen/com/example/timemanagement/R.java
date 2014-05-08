e1a7fb45051dfcbcac97e1c0826a85b322b8a659
FeliciaRingblom <felicia.ringblom@gmail.com>
2014-05-08 10:26:16 +0200

    changed colors to variables

--- TimeManagement/gen/com/example/timemanagement/R.java
+++ TimeManagement/gen/com/example/timemanagement/R.java
@@ -18,6 +18,7 @@ public final class R {
         public static final int green=0x7f060007;
         public static final int lightGrey=0x7f060002;
         public static final int orange=0x7f060000;
+        public static final int red=0x7f060008;
         public static final int title_background=0x7f060003;
     }
     public static final class dimen {
