1a19b85a4148c1e7886630ebd917f955550ca867
August Nam-Ki Ek <augek158@student.liu.se>
2014-05-15 11:48:21 +0200

    removed unused import

--- TimeManagement/src/com/example/timemanagement/TimestampActivity.java
+++ TimeManagement/src/com/example/timemanagement/TimestampActivity.java
@@ -22,7 +22,6 @@ import android.support.v4.util.LogWriter;
 import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
